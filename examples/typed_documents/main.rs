8460189e6159fb70c5fbfd45460a37a702930655
Marcel Ibes <mibes@mibesco.com>
2020-02-04 07:48:21 +0100

    Example on how to work with typed documents.

---
+++ examples/typed_documents/main.rs
@@ -0,0 +1,69 @@
+extern crate sofa;
+
+use sofa::types::document::DocumentId;
+use serde::{Serialize, Deserialize};
+use reqwest::StatusCode;
+
+/// Update DB_HOST to point to your running Couch instance
+const DB_HOST: &'static str = "http://192.168.64.5:5984";
+const TEST_DB: &'static str = "test_db";
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct TestDoc {
+    /// _ids are are the only unique enforced value within CouchDB so you might as well make use of this.
+    /// CouchDB stores its documents in a B+ tree. Each additional or updated document is stored as
+    /// a leaf node, and may require re-writing intermediary and parent nodes. You may be able to take
+    /// advantage of sequencing your own ids more effectively than the automatically generated ids if
+    /// you can arrange them to be sequential yourself.
+    pub _id: DocumentId,
+
+    /// Document Revision, provided by CouchDB, helps negotiating conflicts
+    #[serde(skip_serializing)]
+    pub _rev: String,
+
+    pub first_name: String,
+    pub last_name: String,
+}
+
+fn main() {
+    println!("Connecting...");
+
+    // Prepare the Sofa client
+    let client = sofa::Client::new(DB_HOST).unwrap();
+
+    // This command gets a reference to an existing database, or it creates a new one when it does
+    // not yet exist.
+    let db = client.db(TEST_DB).unwrap();
+
+    let td = TestDoc {
+        _id: "1234".to_string(),
+        _rev: "".to_string(),
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+    };
+
+    // check if the document already exists
+    match db.get("1234".to_string()) {
+        Ok(existing) => {
+            println!("Document has been previously created with Rev: {}", existing._rev);
+            let e: TestDoc = serde_json::from_value(existing.get_data()).unwrap();
+            println!("Name: {} {}", e.first_name, e.last_name);
+        },
+        Err(e) => {
+            match e.status {
+                StatusCode::NOT_FOUND => {
+                    // create the document
+                    match db.create(serde_json::to_value(td).unwrap()) {
+                        Ok(r) => println!("Document was created with ID: {} and Rev: {}", r._id, r._rev),
+                        Err(err) => println!("Oops: {:?}", err),
+                    }
+                }
+                _ => {
+                    println!("Unexpected error: {:?}", e);
+                }
+            }
+        }
+    }
+
+    println!("All operations are done")
+}
