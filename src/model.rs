91bbfdafcecbfaabed1765e6d58629a3963d2c7e
mibes <mibes@mibesco.com>
2020-09-27 19:51:11 +0200

    Use Cow for get_data()

--- src/model.rs
+++ src/model.rs
@@ -1,16 +1,19 @@
+use crate::document::Document;
 use serde::de::DeserializeOwned;
 use serde::ser::Serialize;
 use serde_json::{from_value, to_value};
 use std::marker::Sized;
-use crate::document::Document;
 
 /// Trait that provides methods that can be used to switch between abstract `Document` and concrete `Model` implementors (such as your custom data models)
 pub trait Model<T: Serialize + DeserializeOwned + Sized> {
     fn from_document(d: Document) -> T {
-        from_value(d.get_data()).unwrap()
+        from_value(d.get_data().into_owned()).unwrap()
     }
 
-    fn to_document(&self) -> Document where Self: Serialize {
+    fn to_document(&self) -> Document
+    where
+        Self: Serialize,
+    {
         Document::new(to_value(self).unwrap())
     }
 }
