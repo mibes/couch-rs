01176969c4c0ce09e5deb4b93cdb42e5ae99de68
Horaci Macias <hmacias@avaya.com>
2020-09-27 19:52:09 +0200

    Added test script that starts/stops docker automatically

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+COUCHDB_USER=admin
+COUCHDB_PASSWORD=password
+
+docker_id=$(docker run -d --rm -p 5984:5984 -e COUCHDB_USER=$COUCHDB_USER -e COUCHDB_PASSWORD=$COUCHDB_PASSWORD couchdb:3)
+
+# docker kill if test is interrupted
+trap 'docker kill $docker_id' SIGINT
+
+echo "Waiting for docker couchdb to be up..."
+while ! curl -s -u $COUCHDB_USER:$COUCHDB_PASSWORD http://localhost:5984/; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo "CouchDB is up. Starting tests in 1s"
+sleep 1
+
+cargo test -- --test-threads=1
+
+docker kill "$docker_id"
