910289e02dd052677002755bd443f506ffe4f75f
mibes <mibes@mibesco.com>
2020-10-01 07:48:58 +0200

    Renamed trait conversion

--- couch_rs/src/model.rs
+++ couch_rs/src/model.rs
@@ -2,13 +2,13 @@ use crate::document::TypedCouchDocument;
 use serde::ser::Serialize;
 use serde_json::{from_value, to_value, Value};
 
-/// Trait that provides methods that can be used to switch between abstract `Document` and concrete `Model` implementors (such as your custom data models)
+/// Trait that provides methods that can be used to switch between abstract `Value` and concrete `Model` implementors (such as your custom data models)
 pub trait Model<T: TypedCouchDocument> {
-    fn from_document(d: Value) -> T {
+    fn from_raw(d: Value) -> T {
         from_value(d).unwrap()
     }
 
-    fn to_document(&self) -> Value
+    fn to_raw(&self) -> Value
     where
         Self: Serialize,
     {
